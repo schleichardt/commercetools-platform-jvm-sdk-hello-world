407e0d68453c0ff37a74e536edc656c8d9bf76bd
schleichardt <michael.schleichardt@commercetools.de>
2015-08-11 11:51:25 +0200

    Add logging infrastructure.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import io.sphere.sdk.client.SphereAsyncHttpClientFactory;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientFactory;
+import io.sphere.sdk.products.ProductProjection;
 import io.sphere.sdk.products.queries.ProductProjectionQuery;
+import io.sphere.sdk.queries.PagedQueryResult;
 
-//mvn exec:java
-public class Main {
+import java.util.concurrent.CompletionStage;
+
+//mvn compile exec:java
+public final class Main {
     public static void main(String[] args) {
         final SphereClientFactory factory = SphereClientFactory.of(SphereAsyncHttpClientFactory::create);
         final SphereClient client = factory.createClient(
@@ -12,7 +16,10 @@ public class Main {
                 "fOrRJ6q7fP9gOikBlrHt6Pzi", //replace with your client id
                 "tW3hRMeMldf-36U8DIS0v9K6Mh3Si5P2"); //replace with your client secret
 
-        client.execute(ProductProjectionQuery.ofCurrent()).toCompletableFuture().join().head().ifPresent(System.err::println);
+        final CompletionStage<PagedQueryResult<ProductProjection>> resultCompletionStage =
+                client.execute(ProductProjectionQuery.ofCurrent());
+        final PagedQueryResult<ProductProjection> queryResult = resultCompletionStage.toCompletableFuture().join();
+        queryResult.getResults().forEach(System.err::println);
         client.close();
     }
 }
